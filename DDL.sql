88201a174add43ec70a6ffad35dac794e39728e4
Geraldin <geraldin570.guerrero@gmail.com>
2026-05-24 13:12:32 -0500

    se actualiza DDL con las otras tablas

--- DDL.sql
+++ DDL.sql
@@ -15,3 +15,53 @@ CREATE TABLE usuaio (
 	FOREIGN KEY(ciudad_id)
 	REFERENCES ciudad(ciudad_id)
 ); 
+CREATE TABLE empleado (
+    empleado_id SERIAL PRIMARY KEY,
+    cargo VARCHAR (10), 
+    departamento VARCHAR(10), 
+    usuario_id SERIAL,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuaio(usuario_id)
+);
+
+CREATE TABLE servicio(
+    servicio_id SERIAL PRIMARY KEY, 
+    nombre VARCHAR (20), 
+    descripcion VARCHAR (100), 
+    estado VARCHAR(10)
+);
+
+CREATE TABLE turno(
+    turno_id SERIAL PRIMARY KEY, 
+    usuario_id INT, 
+    empleado_id INT, 
+    servicio_id INT, 
+    hora_inicio TIME
+    hora_fin TIME,
+    estado VARCHAR (20),
+    fecha DATE, 
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuario(usuario_id),
+    FOREIGN KEY(empleado_id)
+    REFERENCES empleado(empleado_id),
+    FOREIGN KEY (servicio_id)
+    REFERENCES servicio(servicio_id)
+);
+
+CREATE TABLE medio_notificacion(
+   medio_notificacion_id SERIAL PRIMARY KEY, 
+   nombre_medio_notificacion VARCHAR
+); 
+
+CREATE TABLE notificacion(
+    notificacion_id SERIAL PRIMARY KEY, 
+    usuario_id INT, 
+    medio_notificacion_id INT, 
+    mensaje VARCHAR(100), 
+    fecha_envio DATE, 
+    estado VARCHAR,
+    FOREIGN KEY(usuario_id)
+    REFERENCES usuario(usuario_id),
+    FOREIGN KEY(medio_notificacion_id)
+    REFERENCES medio_notificacion(medio_notificacion_id)
+); 
